d7dd4212e5ba09bea6f917324737f4687cce5751
matthieu.hanne@gmail.com <bessif@localhost.localdomain>
2014-03-29 03:54:10 +0100

    Bug pagination

--- patentScrapper/patentScrapper/spiders/espacenet.py
+++ patentScrapper/patentScrapper/spiders/espacenet.py
@@ -3,38 +3,47 @@ from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from patentScrapper.items import PatentscrapperItem
 from scrapy.http import Request
+from types import *
 
 class EspacenetSpider(CrawlSpider):
     name = 'espacenet'
-
+    x = 0
     def __init__(self, keywords=None, *args, **kwargs):
         super(EspacenetSpider, self).__init__(*args, **kwargs)
         self.start_urls = ['http://worldwide.espacenet.com/searchResults?compact=false&ST=singleline&query=%s&locale=en_EP&DB=worldwide.espacenet.com' % keywords]
         allowed_domains = ['worldwide.espacenet.com']
         rules = (
-            Rule(SgmlLinkExtractor(), callback='parse_links', follow=True)
+           # Rule(SgmlLinkExtractor(), callback='parse_links', follow=True)
         )
 
-    def parse_links(self, response):
-        print "linnnk"
-        filename = response.url.split("/")[-2] + "parle_links"
+
+    def parse_patent(self, response):
+        sel = Selector(response)
+        i = PatentscrapperItem()
+        self.x+= 1
+        filename = 'patent'+`self.x`
         open(filename, 'wb').write(response.body)
+        i['bookmark'] = sel.xpath("//div[@id='pagebody']/h3/text()").extract()
+        i['inventors'] = 0
+        i['applicants'] = 0
+        i['classification'] = 0
+        i['applicationNumber'] = 0
+        i['priorityNumbers'] = 0
+        i['published'] = 0
+        i['core'] = 0
 
     def parse(self, response):
         sel = Selector(response)
-        i = PatentscrapperItem()
         links = sel.xpath('//a[@class="publicationLinkClass"]/@href').extract()
-        for link in links:
-            yield Request("http://worldwide.com"+link, callback=self.parse)
-            print 'test'
-            open('brvt', 'wb').write(response.body)
-            i['bookmark'] = sel.xpath("//div[@id='pagebody']/h3/text()").extract()
-            i['inventors'] = 0
-            i['applicants'] = 0
-            i['classification'] = 0
-            i['applicationNumber'] = 0
-            i['priorityNumbers'] = 0
-            i['published'] = 0
-            i['core'] = 0
+        #for link in links:
+           #yield Request("http://worldwide.com"+link, callback=self.parse_patent)
+
+        nextPage = sel.xpath('//a[@class="paginationNext"]/@href').extract()
+        print "URL ->    SIZE   " +`len(nextPage)`
+        if type(nextPage) is list:
+            yield Request("http://worldwide.com/"+nextPage[0], callback=self.parse)
+        else:
+            yield Request("http://worldwide.com/"+nextPage, callback=self.parse)
 
         #return i
+
