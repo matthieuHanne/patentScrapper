d3f07c219a9ae9142ad9fd82bd5167cf43a18858
HANNE <hmat@vps61245.ovh.net>
2014-05-06 21:09:52 +0200

    Clean get requests

--- front/app/views/leaf.js
+++ front/app/views/leaf.js
@@ -1,72 +1,57 @@
 /*global define Leaf View*/
 define(['backbone'],
-		function(Backbone){
-			'use strict';
+        function(Backbone){
+            'use strict';
 
-			var view = Backbone.View.extend({
-				'tagName'	: 'section',
-				'initialze'	: function(){
-				},
-				'render'	: function(tpl){
-					this.$el.html(_.template(tpl)); 
-					return this;
-				},
-				'events': {
-					'click .add-keyword'	: 'addKeywordRow',
-				'click .start-search' 	: 'startSearch',
-				'click .start-crawl'	: 'startCrawl',
-				},
-				'addKeywordRow' : function(event){
-					$('.col-xs-3:first-child').clone().appendTo('.keywords');
-				},
-				'startSearch' : function(event){
-					//TODO: Alert
-					//Request to API
-					var keys=[]; 
-					$('.keyword').each(function(){keys.push($(this).val())})
-						var param='';
-					$('.param').each(function(){if($(this).prop('checked')) param +=$(this).val()})
-						var bdd='';
-					$('.bdd').each(function(){if($(this).prop('checked')) bdd +=$(this).val()})
-					
+            var view = Backbone.View.extend({
+                'tagName'	: 'section',
+                'initialze'	: function(){
+                },
+                'render'	: function(tpl){
+                    this.$el.html(_.template(tpl)); 
+                    return this;
+                },
+                'events': {
+                    'click .add-keyword'	: 'addKeywordRow',
+                    'click .start-search' 	: 'startSearch',
+                    'click .start-crawl'	: 'startCrawl',
+                },
+                'addKeywordRow' : function(event){
+                    $('.col-xs-3:first-child').clone().appendTo('.keywords');
+                },
+                'startSearch' : function(event){
+                    //Prepare request content
+                    var keys=[]; 
+                    var param='';
+                    var bdd='';
+                    $('.keyword').each(function(){keys.push($(this).val())});
+                    $('.param').each(function(){if($(this).prop('checked')) param+=$(this).val()});
+                    $('.bdd').each(function(){if($(this).prop('checked')) bdd+=$(this).val()});
+                    //Send get request
+                    $.getJSON($SCRIPT_ROOT + App.config.apiUrl+'/patentapi/export', {
+                        keywords: keys, args: param, mail: this.$el.find('#mail').val()
+                    }, function(data) {
+                        $("#result").text(data.result);
+                    });
+                    return false;
+                },
+                'startCrawl' : function(event){
+                    //Prepare request content
+                    var keys=[]; 
+                    var param='';
+                    var bdd='';
+                    $('.keyword').each(function(){keys.push($(this).val())});
+                    $('.param').each(function(){if($(this).prop('checked')) param+=$(this).val()});
+                    $('.bdd').each(function(){if($(this).prop('checked')) bdd+=$(this).val()});
+                    //Send get request
+                    $.getJSON($SCRIPT_ROOT + App.config.apiUrl+'/patentapi/crawl', {
+                        keywords: keys, args: param, mail: this.$el.find('#mail').val()
+                    }, function(data) {
+                        $("#result").text(data.result);
+                    });
+                    return false;
+                }
+            });
 
-
-						$.ajax({
-							type: 'POST',
-							url: App.config.apiUrl+'/patentapi/export',
-							crossDomain: true,
-							data: JSON.stringify({keywords: keys, args: param, mail: this.$el.find('#mail').val()}),
-							contentType: 'application/json',
-							success: function(responseData, textStatus, jqXHR) {
-								var value = responseData.someKey;
-								App.Router.navigate('dashboard', {trigger: true})
-							},
-							error: function (responseData, textStatus, errorThrown) {
-								console.log( "ERROR:", error );
-
-							}
-						});
-				},
-				'startCrawl' : function(event){
-					//TODO: Alert
-					//Request to API
-					var keys=[]; 
-					$('.keyword').each(function(){keys.push($(this).val())})
-						var param='';
-					$('.param').each(function(){if($(this).prop('checked')) param +=$(this).val()})
-						var bdd='';
-					$('.bdd').each(function(){if($(this).prop('checked')) bdd +=$(this).val()})
-
-						$.post(App.config.apiUrl+'/patentapi/crawl',{'keywords[]': keys, 'args': param, 'target': bdd })
-						.done(function(responseData){
-							App.Router.navigate('dashboard', {trigger: true})
-						})
-					.fail(function() {
-						alert( "Post failed" );
-						App.Router.navigate('dashboard', {trigger: true})
-					});
-				},
-			});
-
-			return view;
-		});
+            return view;
+        });
